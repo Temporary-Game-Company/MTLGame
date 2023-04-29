d81a454b9dc2d2f218e47635bda021e0e303c79d
unknown <mohammadreza.sabramooz.1@ens.etsmtl.ca>
2023-04-29 18:47:17 -0400

    add timer to UHD

--- Assets/MtlGame/Code/Scripts/ScriptableObjects/Events/EventManager.cs
+++ Assets/MtlGame/Code/Scripts/ScriptableObjects/Events/EventManager.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime;
+using UnityEngine.UI;
+using TMPro;
+
 public class NewBehaviourScript : MonoBehaviour
 {
     private float infectionRate=120f;
 
     [SerializeField] List<Home> homes;
     List<Home> unInfectedHomes = new List<Home>();
+    public TMP_Text gameTimerText;
+    private float gameTimer=0f;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +24,16 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         infectHouseRandomly();
+        Timer();
+    }
+
+    void Timer(){
+        gameTimer += Time.deltaTime*1;
+         int seconds = (int)(gameTimer % 60);
+         int minutes = (int)(gameTimer / 60) % 60;
+        int hours = (int)(gameTimer /3600) % 24;
+        string timeString = string.Format("{0:0}:{1:00}:{2:00}",hours,minutes,seconds);
+        gameTimerText.text = timeString;
     }
 
     void infectHouseRandomly(){
