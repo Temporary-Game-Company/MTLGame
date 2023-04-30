3341b997d8805f86ab7fe5cc99017db5292fffc1
GiguereMA <marco.allard.giguere@gmail.com>
2023-04-30 08:18:17 -0400

    Adding SFX to Wwise
    not integrated yet

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
         static const AkUniqueID PLAY_SFX_BELLRINGING = 2577559157U;
     } // namespace EVENTS
 
